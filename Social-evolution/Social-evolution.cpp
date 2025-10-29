bd53520d2a1c7b1f67f2ae6646b3b329850e3cf8
Frigzer <borekadam89@gmail.com>
2025-10-29 11:20:36 +0100

    Change project to cmake

---
+++ Social-evolution/Social-evolution.cpp
@@ -0,0 +1,12 @@
+﻿// Social-evolution.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "Social-evolution.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
