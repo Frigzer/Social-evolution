fa23bcb921bf8d9bb0a65c1ca2e056b7942c9cce
Frigzer <borekadam89@gmail.com>
2025-11-16 18:06:17 +0100

    Create SimulationRenderer.cpp

---
+++ src/SimulationRenderer.cpp
@@ -0,0 +1,20 @@
+#include "SimulationRenderer.hpp"
+
+SimulationRenderer::SimulationRenderer(Simulation& s)
+    : sim(s)
+{
+    cellSize = 800.0f / sim.grid.width;
+}
+
+void SimulationRenderer::draw(sf::RenderWindow& win) {
+
+    sf::RectangleShape cell({ cellSize - 1.f, cellSize - 1.f });
+
+    for (int y = 0; y < sim.grid.height; ++y) {
+        for (int x = 0; x < sim.grid.width; ++x) {
+            cell.setPosition({ x * cellSize, y * cellSize });
+            cell.setFillColor(sim.grid.get(x, y).getColor());
+            win.draw(cell);
+        }
+    }
+}
