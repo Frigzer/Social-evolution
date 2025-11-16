6971d8bbae97ff8e350333e010d73aec8a7603c0
Frigzer <borekadam89@gmail.com>
2025-11-16 18:06:15 +0100

    Create SimulationApp.cpp

---
+++ src/SimulationApp.cpp
@@ -0,0 +1,37 @@
+#include "SimulationApp.hpp"
+
+SimulationApp::SimulationApp()
+    : window(sf::VideoMode({ 1100, 800 }), "Ewolucja zachowan spolecznych"),
+    sim(50, 50, { 3, 5, 0, 1 }),
+    renderer(sim),
+    gui(sim, running)
+{
+    window.setFramerateLimit(30);
+    ImGui::SFML::Init(window);
+}
+
+SimulationApp::~SimulationApp() {
+    ImGui::SFML::Shutdown();
+}
+
+void SimulationApp::run() {
+
+    while (window.isOpen()) {
+
+        while (const std::optional event = window.pollEvent()) {
+            ImGui::SFML::ProcessEvent(window, *event);
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        gui.update(window);
+        if (running) sim.step();
+
+        window.clear();
+        renderer.draw(window);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+}
