0cc1daca7dfcf5ed988d5a66c04304168d74f022
Frigzer <borekadam89@gmail.com>
2025-11-19 19:21:30 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
-﻿#include "SimulationApp.hpp"
+﻿#define NOMINMAX
+#include <Windows.h>
+#include "SimulationApp.hpp"
 
 int main() {
+    SetProcessDPIAware();
     SimulationApp app;
     app.run();
     return 0;
