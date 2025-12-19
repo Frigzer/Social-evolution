408608a069377bc4141fa48570f547225e4e4381
Adam Borek <borekadam89@gmail.com>
2025-12-19 16:23:27 +0100

    Create LeftPanel.hpp

---
+++ include/LeftPanel.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include "Simulation.hpp"
+#include "SimulationRenderer.hpp"
+
+
+
+class LeftPanel {
+public:
+    LeftPanel(Simulation& sim, SimulationRenderer& renderer);
+
+    void setSize(sf::Vector2u size); // np. 800x800
+    void setMode(LeftPanelMode m);
+    LeftPanelMode getMode() const;
+
+    // rysuje lewy panel (ImGui okno bez ramek) i zawartoœæ
+    void draw();
+
+private:
+    Simulation& sim;
+    SimulationRenderer& renderer;
+
+    LeftPanelMode mode = LeftPanelMode::Simulation;
+
+    sf::RenderTexture mapTexture;
+
+    // helpery do wykresów (bufory robocze)
+    void drawSimulationView();
+    void drawMetricsView();
+
+    // dla „przesuwaj¹cego siê wykresu”:
+    int plotWindow = 600; // ile ostatnich próbek pokazujemy
+};
